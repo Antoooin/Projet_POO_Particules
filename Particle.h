2403b7c82af0897131a4423632f5138bebd9aaa2
Antoooin <antoine.pagot005@gmail.com>
2026-02-06 17:05:19 +0100

    Création du struct Particle à ne plus toucher

---
+++ Particle.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cmath>
+#include "Vec2.h"
+
+struct Particle{
+    Vec2 position;
+    Vec2 velocity;
+    //On initialise Particle.force à 0,0 pour s'éviter des bugs
+    Vec2 force{};
+    double mass=1.0;
+
+    void resetForce(){
+        force=Vec2{};
+    }
+};
